25a31c82c5921324366f2f40242b6ff56d555691
Jin <chlcjfwls1@naver.com>
2025-03-21 13:27:22 +0900

    ccj) 역방향 부유 이펙트 속도 동기화

--- Assets/Demo/DemoSj/Scripts/BackGroundController.cs
+++ Assets/Demo/DemoSj/Scripts/BackGroundController.cs
@@ -41,7 +41,10 @@ namespace SkyDragonHunter.Game {
         {
             foreach (var obj in floatingObjects)
             {
-                obj.floatSpeedOffset = scrollSpeed;
+                if (scrollSpeed <= float.MaxValue && scrollSpeed >= 0f)
+                    obj.floatSpeedOffset = Mathf.Clamp(scrollSpeed, 1f, float.MaxValue);
+                else if (scrollSpeed <= 0f && scrollSpeed >= -1f * float.MaxValue)
+                    obj.floatSpeedOffset = Mathf.Clamp(scrollSpeed * -1f, 1f, float.MaxValue);
             }
         }
         // Others
