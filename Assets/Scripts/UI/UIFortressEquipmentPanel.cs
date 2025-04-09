d717bf3e92a33ca57d028c5d122a931b0076db4b
Jin <chlcjfwls1@naver.com>
2025-04-09 10:44:46 +0900

    ccj) 단원 초상화 표시 및 우클릭 장착 해제 기능 추가

---
+++ Assets/Scripts/UI/UIFortressEquipmentPanel.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SkyDragonHunter {
+    public class UIFortressEquipmentPanel : MonoBehaviour
+    {
+        // 필드 (Fields)
+        public Image[] mountSlotIcons;
+
+        // 속성 (Properties)
+        // 외부 종속성 필드 (External dependencies field)
+        // 이벤트 (Events)
+        // 유니티 (MonoBehaviour 기본 메서드)
+        // Public 메서드
+        // Private 메서드
+        // Others
+    
+    } // Scope by class UIFortressEquipmentPanel
+} // namespace SkyDragonHunter
