ea343fe6782309a68fcb5e176c29a6afe5aa9069
이재혁 <jhl930303@naver.com>
2025-05-12 15:34:37 +0900

    ljh) Tutorial Save 적용

--- Assets/Scripts/SaveLoad/SavedTutorialData.cs
+++ Assets/Scripts/SaveLoad/SavedTutorialData.cs
@@ -8,37 +8,23 @@ using System.Collections.Generic;
 
 namespace SkyDragonHunter.SaveLoad
 {
-    public class SavedTutorial
-    {
-        public bool oneClear;
-        public bool isStartTutorial;
-        public bool tutorialEnd;
-    }
-
     public class SavedTutorialData
     {
-        public SavedTutorial savedTutorial;
+        public bool tutorialCleared;
 
         public void InitData()
         {
-            savedTutorial = new SavedTutorial();
-            savedTutorial.oneClear = false;
-            savedTutorial.isStartTutorial = true;
-            savedTutorial.tutorialEnd = false;
+            tutorialCleared = false;
         }
 
         public void UpdateSavedData()
         {
-            savedTutorial.oneClear = TutorialEndValue.OneClear;
-            savedTutorial.isStartTutorial = TutorialEndValue.IsStartTutorial;
-            savedTutorial.tutorialEnd = TutorialEndValue.TutorialEnd;
+
         }
 
         public void ApplySavedData()
         {
-            TutorialEndValue.OneClear = savedTutorial.oneClear;
-            TutorialEndValue.IsStartTutorial = savedTutorial.isStartTutorial;
-            TutorialEndValue.TutorialEnd = savedTutorial.tutorialEnd;
+
         }
 
     } // Scope by class SavedItemData
